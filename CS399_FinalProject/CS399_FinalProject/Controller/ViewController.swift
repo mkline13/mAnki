6d5316f03bcd22975e4ea7f83c38b8506c6c00f5
Mason Kline <mkline13@gmail.com>
2022-07-27 15:46:21 -0700

    created xcode project

---
+++ CS399_FinalProject/CS399_FinalProject/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS399_FinalProject
+//
+//  Created by Work on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
