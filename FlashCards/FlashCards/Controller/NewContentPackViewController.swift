58976bf508b262394d995dacb3079fbd29cca36a
Mason Kline <mkline13@gmail.com>
2022-08-03 14:43:53 -0700

    not working, trying new form strategy

--- FlashCards/FlashCards/Controller/NewContentPackViewController.swift
+++ FlashCards/FlashCards/Controller/NewContentPackViewController.swift
@@ -30,26 +30,38 @@ class NewContentPackViewController: UIViewController, UITableViewDataSource, UIT
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell: UITableViewCell
+        let offscreen = self.view.bounds.size.width
+        let inset: CGFloat = 15
         
         switch Row.allCases[indexPath.row] as Row {
+        case .spacer:
+            let spacerCell = UITableViewCell()
+            cell = spacerCell
+            NSLayoutConstraint.activate([cell.heightAnchor.constraint(equalToConstant: 10.0)])
+            cell.separatorInset = UIEdgeInsets(top: 0, left: offscreen, bottom: 0, right: 0)
         case .title:
             let textEntryCell = tableView.dequeueReusableCell(withIdentifier: TextEntryCell.cellIdentifier, for: indexPath) as! TextEntryCell
             textEntryCell.configure(fieldIdentifier: "title")
             cell = textEntryCell
+            cell.separatorInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
         case .author:
             let textEntryCell = tableView.dequeueReusableCell(withIdentifier: TextEntryCell.cellIdentifier, for: indexPath) as! TextEntryCell
             textEntryCell.configure(fieldIdentifier: "author")
             cell = textEntryCell
+            cell.separatorInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
         case .description:
             let multilineTextEntryCell = tableView.dequeueReusableCell(withIdentifier: MultilineTextEntryCell.cellIdentifier, for: indexPath) as! MultilineTextEntryCell
             multilineTextEntryCell.configure(fieldIdentifier: "description")
             cell = multilineTextEntryCell
+            NSLayoutConstraint.activate([cell.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0.0)])
+            cell.separatorInset = UIEdgeInsets(top: 0, left: offscreen, bottom: 0, right: 0)
         }
 
         return cell
     }
-    
+        
     enum Row: CaseIterable {
+        case spacer
         case title
         case author
         case description
